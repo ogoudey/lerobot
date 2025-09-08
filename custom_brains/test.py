9d0ed480247780e794e3d283ccf132436f85e744
Olin <olin.goog@gmail.com>
2025-09-08 08:17:47 -0400

    testing

--- custom_brains/test.py
+++ custom_brains/test.py
@@ -255,7 +255,7 @@ def record_dataset(dataset_name="dataset3"):
 def test_policy():
     """ Runs the SmolVLA policy at policy_path. Finicky, not working fully. """
     _init_rerun(session_name="smolvla")
-    policy_path = "/home/olin/Robotics/Projects/LeRobot/lerobot/outputs/train/2025-09-06/14-21-15_smolvla/checkpoints/004000/pretrained_model"
+    policy_path = "/home/olin/Robotics/Projects/LeRobot/lerobot/outputs/train/2025-09-06/14-21-15_smolvla/checkpoints/last/pretrained_model"
     #policy_path = "lerobot/smolvla_base"
     robot_config = SO101FollowerConfig(
         port="/dev/ttyACM0",
